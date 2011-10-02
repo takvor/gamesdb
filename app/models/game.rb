69e7107fac92507faaea83cfab886919a46191e5
Tyler B <takvor@gmail.com>
2011-10-02 03:54:36 -0400

    changed some view settings

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  #validates :title, :players, :presence => true
+  validates :title, :players, :presence => true
+  validates :players, :numericality => {:only_integer => :true}
 end
